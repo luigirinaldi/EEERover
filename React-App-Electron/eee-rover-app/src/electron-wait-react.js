665470a3d3f0d4deb5009a02dac0b1e2c43b43dd
Luigi <luigirinaldi06@gmail.com>
2022-06-14 09:29:15 +0100

    changed dev environment so only electron app starts up now

---
+++ React-App-Electron/eee-rover-app/src/electron-wait-react.js
@@ -0,0 +1,28 @@
+const net = require('net');
+const port = process.env.PORT ? (process.env.PORT - 100) : 5000;
+
+// NOTE: Foreman will offset the port number by 100 for processes of different types. (See here.) 
+// So, electron-wait-react.js subtracts 100 to set the port number of the React dev server correctly.
+
+process.env.ELECTRON_START_URL = `http://localhost:${port}`;
+
+const client = new net.Socket();
+
+let startedElectron = false;
+const tryConnection = () => client.connect({port: port}, () => {
+        client.end();
+        if(!startedElectron) {
+            console.log('starting electron');
+            startedElectron = true;
+            const exec = require('child_process').exec;
+            exec('npm run electron');
+        }
+    }
+);
+
+console.log("port is:" + port);
+tryConnection();
+
+client.on('error', (error) => {
+    setTimeout(tryConnection, 1000);
+});
--- React-App-Electron/eee-rover-app/src/pages/ErrorLogs.js
+++ React-App-Electron/eee-rover-app/src/pages/ErrorLogs.js
@@ -2,36 +2,42 @@ import React, { useContext } from 'react';
 import { IpContext } from '../context/ip-context';
 import PageContainer from './PageContainer';
 
-const electron = window.require('electron');
-const { ipcRenderer } = electron;
-// const remote = electron.remote;
-// const { os } = remote;
+// const electron = window.require('electron');
+// const { ipcRenderer } = electron;
+// // const remote = electron.remote;
+// // const { os } = remote;
 
-ipcRenderer.on('asynchronous-reply', (event, arg) => {
-  console.log(event);
-  console.log(arg) // prints "pong"
-})
+// ipcRenderer.on('asynchronous-reply', (event, arg) => {
+//   console.log(event);
+//   console.log(arg) // prints "pong"
+// })
 
 
-const ErrorLogs = () => {  
-  const { roverIP } = useContext(IpContext);
+// const ErrorLogs = () => {  
+//   const { roverIP } = useContext(IpContext);
 
-  return (
-    <PageContainer title="Error Logs">
-      <button onClick={() => {
-        ipcRenderer.send('send-udp-message', {
-          ip: roverIP,
-          port: 1883,
-          host: window.location.host,
-          message: "test",
-        });
-      }}>
-        Click me
-      </button>
-    </PageContainer>
-  );
-};
-  
-// ErrorLogs.contextType = IpContext;
+//   return (
+//     <PageContainer title="Error Logs">
+//       <button onClick={() => {
+//         ipcRenderer.send('send-udp-message', {
+//           ip: roverIP,
+//           port: 1883,
+//           host: window.location.host,
+//           message: "test",
+//         });
+//       }}>
+//         Click me
+//       </button>
+//     </PageContainer>
+//   );
+// };
+const ErrorLogs = () => {  const ErrorLogs = () => {  
+return (
+      <PageContainer title="Error Logs">
+      </PageContainer>
+    );
+  };
+}
+
 
 export default ErrorLogs;
